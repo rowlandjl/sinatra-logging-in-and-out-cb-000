4f6744328a268fed1f57e2d83a513e329a6fb973
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-26 13:16:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    if @user 
+    if !@user.nil? && 
       session[:user_id] = @user.id 
       redirect to '/account'
     end 
