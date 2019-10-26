576327cce4dc23549107ffe1d900921d53f8341f
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-26 12:55:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,9 @@ class ApplicationController < Sinatra::Base
     if @user 
       session[:user_id] = @user.id 
       redirect to '/account'
+    else 
+      erb :error 
     end 
-    redirect 
   end
 
   get '/account' do
