864d3f7445069305d5ea2893474cc22473ee6418
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-26 13:19:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,8 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     @current_user = User.find_by_id(session[:user_id])
-    erb :account 
+    if @current_user
+      erb :account 
   end
 
   get '/logout' do
