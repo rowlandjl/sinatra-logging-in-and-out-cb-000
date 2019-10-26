9caf736bd866f7a99f0f8d7cd589aed18633330b
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-26 12:52:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(params[:username])
+    @user = User.find_by(usrname: params[:username])
   end
 
   get '/account' do
